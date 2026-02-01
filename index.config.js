df4ef6b0e1306ca65633b97b794b30d37108797a
4TVBox <130707302+4TVBox@users.noreply.github.com>
2026-01-31 19:55:39 -0500

    Add files via upload

--- index.config.js
+++ index.config.js
@@ -30,7 +30,7 @@ var index_config_default = {
     list: []
   },
   danmu: {
-    urls: [{ address: "https://fjj0417.dpdns.org/87654321", name: "默认服务器1" }, { address: "https://danmu.14812910.xyz/87654321", name: "默认服务器2" }, { address: "https://dm.stardm.us.kg/87654321", name: "默认服务器3" }],
+    urls: [{ address: "https://fjj0417.dpdns.org/87654321", name: "默认服务器1" }, { address: "https://313236.xyz/87654321", name: "默认服务器2" }, { address: "https://danmu.14812910.xyz/87654321", name: "默认服务器3" }],
     autoPush: true
   },
   color: [
